5140125e30e74e850af151fb7c278556cf6bfa2e
Virginia Wilcox <wilcoxva@gmail.com>
2020-04-14 19:54:06 -0400

    completed validateUserId

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code away!
+const express = require("express")
+const cors = require("cors")
+const logger = require("./middleware/logger")
+const welcomeRouter = require("./welcome/welcome-router")
+const usersRouter = require("./users/users-router")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+server.use(cors())
+
+server.use(logger)
+
+server.use("/", welcomeRouter)
+server.use("/users", usersRouter)
+
+server.use((req, res) => {
+	res.status(404).json({
+		message: "Route was not found",
+	})
+})
+
+server.use((error, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const users = require("../users/userDb")
+const validateUserId = require("../middleware/validateUserId")
 
 const router = express.Router();
 
